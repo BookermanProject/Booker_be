8faceb737bcb05c777d876bdf504c4b25bb612ed
kafka <jjk5158@naver.com>
2023-05-19 17:01:39 +0900

    Init

---
+++ src/main/java/com/sparta/booker/BookerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/booker/BookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
