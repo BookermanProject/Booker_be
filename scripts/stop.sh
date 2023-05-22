3ef6504807ebe807701e0a927a2c34cf33d842a3
김지승 <jjk5158@naver.com>
2023-05-22 12:02:34 +0900

    Update stop.sh
    배포파일 업데이트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
-JAR="$ROOT_PATH/application.jar"
+JAR="$ROOT_PATH/Booker-0.0.1-SNAPSHOT.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
