cad877cb8f7d00b9a81d8e83dc980f16ea890611
kafka <jjk5158@naver.com>
2023-05-29 23:45:19 +0900

    RENAME : SET kafka

--- src/main/java/com/sparta/booker/kafka/controller/KafkaController.java
+++ src/main/java/com/sparta/booker/kafka/controller/KafkaController.java
@@ -1,12 +1,14 @@
 package com.sparta.booker.kafka.controller;
 
 
-import com.sparta.booker.service.KafkaProducer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.booker.kafka.service.KafkaProducer;
 
 @RestController
 public class KafkaController {
