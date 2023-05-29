dc90d7c5870e342558593aed45d9d3978fe85e46
kafka <jjk5158@naver.com>
2023-05-29 23:38:07 +0900

    CHORE : MERGE

--- src/main/java/com/sparta/booker/kafka/controller/KafkaController.java
+++ src/main/java/com/sparta/booker/kafka/controller/KafkaController.java
@@ -1,26 +1,20 @@
-//package com.sparta.booker.controller;
-//
-//import com.sparta.booker.service.KafkaProducer;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping(value = "/kafka/test")
-//public class KafkaController {
-//    private final KafkaProducer producer;
-//
-//    @Autowired
-//    KafkaController(KafkaProducer producer) {
-//        this.producer = producer;
-//    }
-//
-//    @PostMapping(value = "/message")
-//    public String sendMessage(@RequestParam("message") String message) {
-//        this.producer.sendMessage(message);
-//
-//        return "success";
-//    }
-//}
+package com.sparta.booker.controller;
+
+import com.sparta.booker.service.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+	@Autowired
+	private KafkaProducer producer;
+
+	@PostMapping(value = "/sendMessage")
+	public void sendMessage(String message) {
+		producer.sendMessage(message);
+	}
+}
