c1e871718c52ec550bf6e6b13a64bc21a877bf54
JEON <jwodn123@naver.com>
2023-06-02 11:04:43 +0900

    fix : Modify kafka logic
    - 사용자가 이벤트 신청 시, 이벤트 아이디와 사용자 아이디, 신청 시간을 이벤트 메시지로 전송.
    - 프로듀서와 컨슈머를 통해 이벤트 메시지 주고 받는 것까지 확인.

--- src/main/java/com/sparta/booker/kafka/controller/KafkaController.java
+++ src/main/java/com/sparta/booker/kafka/controller/KafkaController.java
@@ -1,23 +1,18 @@
-package com.sparta.booker.kafka.controller;
-
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.sparta.booker.kafka.service.KafkaProducer;
-
-@RestController
-public class KafkaController {
-
-	@Autowired
-	private KafkaProducer producer;
-
-	@PostMapping(value = "/sendMessage")
-	public void sendMessage(String message) {
-		producer.sendMessage(message);
-	}
-}
+//package com.sparta.booker.kafka.controller;
+//
+//import com.sparta.booker.kafka.service.KafkaProducer;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class KafkaController {
+//
+//	@Autowired
+//	private KafkaProducer producer;
+//
+//	@PostMapping(value = "/sendMessage")
+//	public void sendMessage(String message) {
+//		producer.sendMessage(message);
+//	}
+//}
