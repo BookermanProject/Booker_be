cbd15670bc3796b7d7443b0debde23ab6cb3fe8e
UMK <haga5032@naver.com>
2023-06-21 12:54:41 +0900

    실시간 검색어 좋아요 순위 뷰페이지 기능 구현 완료

--- src/main/java/com/sparta/booker/domain/search/elastic/controller/ElasticSearchBookController.java
+++ src/main/java/com/sparta/booker/domain/search/elastic/controller/ElasticSearchBookController.java
@@ -3,10 +3,7 @@ package com.sparta.booker.domain.search.elastic.controller;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.sparta.booker.domain.search.elastic.dto.BookFilterDto;
 import com.sparta.booker.domain.search.elastic.dto.BookListDto;
@@ -24,7 +21,8 @@ public class ElasticSearchBookController {
 	private final ElasticSearchBookService elasticSearchBookService;
 
 	//필터검색
-	@GetMapping("/search/word")
+	@PostMapping("/search/word")
+	@ResponseBody
 	public BookListDto searchWord(@RequestBody BookFilterDto filterDto , Pageable pageable) {
 		return elasticSearchBookService.searchWordByElastic(filterDto, pageable);
 	}
