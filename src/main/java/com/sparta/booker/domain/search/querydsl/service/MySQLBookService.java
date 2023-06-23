d962358441175269114a9ad40f914fad88c08200
kafka <jjk5158@naver.com>
2023-06-23 19:35:22 +0900

    CHRE : merge

--- src/main/java/com/sparta/booker/domain/search/querydsl/service/MySQLBookService.java
+++ src/main/java/com/sparta/booker/domain/search/querydsl/service/MySQLBookService.java
@@ -13,9 +13,7 @@ import com.sparta.booker.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
