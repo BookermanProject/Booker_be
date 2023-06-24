af07b311c1ce0c9caa3bbe70b96cc72e0e706e0c
JEON <jwodn123@naver.com>
2023-06-24 21:13:42 +0900

    fix : Modify kafka
    -유효성 검사 로직을 프로듀서 단에서 처리하도록 변경

--- src/main/java/com/sparta/booker/domain/event/controller/EventController.java
+++ src/main/java/com/sparta/booker/domain/event/controller/EventController.java
@@ -7,6 +7,7 @@ import com.sparta.booker.domain.event.service.EventService;
 import com.sparta.booker.domain.user.dto.ResponseDto;
 import com.sparta.booker.domain.user.entity.User;
 import com.sparta.booker.domain.kafka.service.KafkaProducer;
+import com.sparta.booker.domain.user.util.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,14 +23,14 @@ public class EventController {
 
     //이벤트 등록 API
     @PostMapping("/event")
-    public ResponseEntity<EventResponseDto> createEvent(@RequestBody EventRequestDto eventRequestDto, @AuthenticationPrincipal User user) {
-        return eventService.createEvent(eventRequestDto, user);
+    public ResponseEntity<EventResponseDto> createEvent(@RequestBody EventRequestDto eventRequestDto, @AuthenticationPrincipal UserDetailsImpl user) {
+        return eventService.createEvent(eventRequestDto, user.getUser());
     }
 
     //이벤트 신청 API
     @PostMapping("/event/{eventId}")
-    public ResponseEntity<EventResponseDto> applyEvent(@PathVariable Long eventId, @AuthenticationPrincipal User user) {
-        return kafkaProducer.produceMessage(eventId, user);
+    public ResponseEntity<EventResponseDto> applyEvent(@PathVariable Long eventId, @AuthenticationPrincipal UserDetailsImpl user) {
+        return kafkaProducer.produceMessage(eventId, user.getUser());
         //return ResponseEntity.ok().body(new ResponseDto("이벤트 신청 완료"));
     }
 
